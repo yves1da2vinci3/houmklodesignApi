7780bae8cfc29fe7424f7d79a6dc950fd3ab87f9
yves1da2vinci3 <diomadelacorano@gmail.com>
2022-12-19 08:49:37 +0000

    correct import

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
 import cors from 'cors'
-import {Server} from 'socket.io'
 import http from 'http'
-import Io from './socket.js'
 let app = express()
 const PORT = process.env.PORT || 5000;
 const server = http.createServer(app)
@@ -18,7 +16,6 @@ const server = http.createServer(app)
 //   )
 
 
-const io = Io.init(server)
   // deifition du like handler
 
   // verifie si le mec existe ,si oui retourne un tableau ou tul'enleves lui sinon mets le a l'interieur
@@ -32,23 +29,14 @@ import publishRoutes from './routes/publishRoutes.js'
 
 // connection a la  db
 dotenv.config()
-const connectToMongo = async() => {
-  await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-  });
-  return mongoose;
-};
-await connectToMongo().then(async() => console.log('connected TO MONGO DataBase'));
+
+
 // apply des mmiddlewares
 app.use(cors({origin:"*"}))
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
 
-// real time thing
-io.on('connection',(socket)=>{
-  console.log(`socket ${socket.id} is connected`)
-})
+
 // Routage 
 
 
@@ -63,6 +51,21 @@ app.use('/api/articles',ArticlesRoutes)
 app.use('/api/team',teamRoutes)
 
 app.use('/api/publish',publishRoutes)
-server.listen(PORT,(req,res) => {
+
+
+const connectToMongo = async() => {
+  await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+  });
+  return mongoose;
+};
+await connectToMongo().then(async() => {
+  server.listen(PORT,(req,res) => {
     console.log('running on')
 })
+  console.log('connected TO MONGO DataBase')
+
+}
+
+);
